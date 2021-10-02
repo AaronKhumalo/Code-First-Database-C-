6fc2da84abdcff6a9ee0f19d01d3fc46ec9a64de
AaronKhumalo <aaronkhumalo885@gmail.com>
2021-10-02 10:04:49 +0200

    Code First Database using C#

---
+++ MediClinic/Controllers/RecordsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MediClinic.Models;
+
+namespace MediClinic.Controllers
+{
+    public class RecordsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Records
+        public ActionResult Index()
+        {
+            return View(db.PatientRecords.ToList());
+        }
+
+        // GET: Records/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // GET: Records/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Records/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PatientRecordID,PatientFullname,PatientIDNumber,PatientPrescription,PatientAge")] PatientRecord patientRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PatientRecords.Add(patientRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patientRecord);
+        }
+
+        // GET: Records/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // POST: Records/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PatientRecordID,PatientFullname,PatientIDNumber,PatientPrescription,PatientAge")] PatientRecord patientRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patientRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patientRecord);
+        }
+
+        // GET: Records/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // POST: Records/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            db.PatientRecords.Remove(patientRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MediClinic/Models/PatientRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediClinic.Models
+{
+    public class PatientRecord
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PatientRecordID { get; set; }
+
+        [Required]
+        [DisplayName("FullName")]
+        public string PatientFullname { get; set; }
+
+        [Required(ErrorMessage = "Your ID No must contain 13 digits")]
+        [StringLength(13)]
+        [DisplayName("ID Number")]
+        public string PatientIDNumber { get; set; }
+
+        [Required(ErrorMessage = "Provide the prescription")]
+        [DisplayName("Prescription")]
+        public string PatientPrescription { get; set; }
+
+
+        [Required(ErrorMessage = "Provide the age of the patient")]
+        [DisplayName("Age")]
+        public int PatientAge { get; set; }
+    }
+}
